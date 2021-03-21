0f8b2ec028870052183fc919a8b5213603ade005
Adit <aditnegi1@gmail.com>
2021-03-21 16:00:48 +0530

    add view and create api end points for product service

---
+++ products/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('products', ProductViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('products/<int:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .serializers import *
+from .models import *
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        print(request.data)
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk):
+        pass
+
+    def update(self, request, pk):
+        pass
+
+    def destroy(self, request, pk):
+        pass
