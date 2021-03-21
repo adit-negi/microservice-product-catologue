b7c6a6b3c07c5f663c1f4cad38476e1307c65bd4
Adit <aditnegi1@gmail.com>
2021-03-21 16:13:45 +0530

    add update/delete api endpoints

--- products/views.py
+++ products/views.py
@@ -20,10 +20,26 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk):
-        pass
+        try:
+            product = Product.objects.get(pk=pk)
+        except:
+            return Response(status=400)
+        serializer = ProductSerializer(product, read_only=True)
+        return Response(serializer.data, status=201)
 
     def update(self, request, pk):
-        pass
+        try:
+            product = Product.objects.get(pk=pk)
+        except:
+            return Response(status=400)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=202)
 
     def destroy(self, request, pk):
-        pass
+        try:
+            product = Product.objects.get(pk=pk).delete()
+        except:
+            return Response(status=400)
+        return Response(status=204)
